7302934343f010dfa1751e6ece69f38f88df0924
yurim-hgr <dbfla4618@naver.com>
2020-08-25 12:55:50 +0900

    modify multiprocess-speedtest.py

--- multiprocess-speedtest.py
+++ multiprocess-speedtest.py
@@ -4,7 +4,7 @@ import time
 start_time = time.time()
 
 def count( name ):
-    for i in range( 1, 50001 ):
+    for i in range( 1, 501 ):
         print( name, ' : ', i )
 
 num_list = [ 'p1', 'p2', 'p3', 'p4' ]
